d8558c3e7d6adc8da687040b939080cd9680b9b7
tokuyama shouta <toku1031ts@gmail.com>
2020-10-04 14:41:05 +0900

    commit

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 'use strict';
+//10の質問
+//質問を選ぶと次の質問へ
+//yesの数で採点
+
+
 
 {
   const question = document.getElementById('question'); //ID取得
@@ -6,11 +11,20 @@
   const btn = document.getElementById('btn'); //ID取得
   const result = document.getElementById('result'); //ID取得
   const scoreLabel = document.querySelector('#result > p');
+  
 
   const quizSet = shuffle([ //問題の配列
-    {q:'世界で一番大きな湖は？',c:['カスピ海','カリブ海','琵琶湖']},
-    {q:'2の8乗は?',c:['256','64','1024']},
-    {q:'次のうち、最初にリリースされた言語は？?',c:['Python','JavaScript','HTML']},
+    {q:'休みの日は何してるの？と聞かれた',c:['Yes','No']},
+    {q:'好きな食べ物、苦手な食べ物を聞かれた',c:['Yes','No']},
+    {q:'今度の休みの予定を聞かれた',c:['Yes','No']},
+    {q:'クリスマスの予定を聞かれた?',c:['Yes','No']},
+    {q:'小さい時はどんな子供だったの？と聞かれた?',c:['Yes','No']},
+    {q:'どんなタイプの女性が好きなの？と聞かれた?',c:['Yes','No']},
+    {q:'趣味に関する細かい質問をされた',c:['Yes','No']},
+    {q:'出身地や生い立ちに関する質問をされた?',c:['Yes','No']},
+    {q:'親や兄弟など家族に関する質問をされた?',c:['Yes','No']},
+    {q:'血液型は何？と聞かれた',c:['Yes','No']},
+    
   ]);
   let currentNum = 0; //今何問目のクイズを説いているか
   let isAnswered;
@@ -19,9 +33,10 @@
   
 
   function shuffle(arr) {
-    
-    for(let i = arr.length -1; i > 0; i--){ //ランダムに選ぶ終点のインデックスi
-      const j = Math.floor(Math.random() * (i + 1)); //配列のなかからランダムに要素を選ぶ0〜iばんめ
+    //ランダムに選ぶ終点のインデックスi
+    for(let i = arr.length -1; i > 0; i--){ 
+      //配列のなかからランダムに要素を選ぶ0〜iばんめ
+      const j = Math.floor(Math.random() * (i + 1)); 
       [arr[j],arr[i]] = [arr[i],arr[j]];
     }
     return arr;
@@ -33,10 +48,14 @@
     }
     isAnswered = true;
 
+    //もし選ばれたコンテントが答えの配列の一番始めなら
     if(li.textContent === quizSet[currentNum].c[0]){
+      //選択した時に色をつけるcorrectクラスをつける
       li.classList.add('correct')
+      //YESの数だけスコアを足す
       score++;
     }else{
+      //選択した時に色をつけるcorrectクラスをつける
       li.classList.add('wrong');
     }
 
@@ -47,23 +66,27 @@
 
   function setQuiz(){
     isAnswered = false;
-    question.textContent = quizSet[currentNum].q;//問題を配列から表示
+    //問題を配列から表示
+    question.textContent = quizSet[currentNum].q;
 
     while(choices.firstChild){
       choices.removeChild(choices.firstChild);
     }
 
-    const shuffledChoices = shuffle([...quizSet[currentNum].c]);
+    const shuffledChoices = [...quizSet[currentNum].c];
     
     shuffledChoices.forEach(choice => {
-      const li = document.createElement('li'); //liを作成
-      li.textContent = choice; //liを表示
+      //liを作成
+      const li = document.createElement('li'); 
+      //liを表示
+      li.textContent = choice; 
       li.addEventListener('click',() => {
         checkAnswer(li);
       });
       choices.appendChild(li);
     });
 
+    //問題が終わるとスコア表示
     if(currentNum === quizSet.length -1){
       btn.textContent = 'Show Score';
     }
@@ -79,7 +102,13 @@
 
     if(currentNum === quizSet.length -1){
       // console.log(`Score:${score} / ${quizSet.length}`);
-      scoreLabel.textContent = `Score:${score} / ${quizSet.length}`;
+      if(score >= 7){
+        scoreLabel.textContent = `脈ありです！告白しよう！Score:${score} / ${quizSet.length}`
+      }else if(score >= 4 && score < 7){
+        scoreLabel.textContent = `後少し頑張ってみて！Score:${score} / ${quizSet.length}`
+      }else{
+        scoreLabel.textContent = `脈なしです！次の恋へ！Score:${score} / ${quizSet.length}`
+      }
       result.classList.remove('hidden');
     }else{
       currentNum++;
@@ -87,6 +116,17 @@
       
     }
   });
+
+  let startBtn = document.getElementById('startBtn');
+  let box = document.getElementById('box');
+
+  startBtn.addEventListener('click', function(){
+    box.classList.add('fadeout');
+    setTimeout(function(){ 
+      box.style.display = "none"; 
+    }, 1000);
+  }, false);
+  
 
 
 
